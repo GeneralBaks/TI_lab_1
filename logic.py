dd731b2d62d9c6e94e37463d59411742eae23f02
GeneralBaks <vlad2273845@gmail.com>
2026-02-14 21:55:07 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,7 +1,10 @@
-from global_val import CYRILLIC, CYRILLIC_LEN
+from global_val import CYRILLIC, CYRILLIC_LEN, ENGLISH, ENGLISH_LEN
 
-def get_weight(c: str) -> int:
+def get_weight_cyrillic(c: str) -> int:
     return CYRILLIC.index(c.lower())
+
+def get_weight_english(c: str) -> int:
+    return ENGLISH.index(c.lower())
 
 def gcd(a: int, b: int) -> int:
     while b:
@@ -10,24 +13,72 @@ def gcd(a: int, b: int) -> int:
 
 def cipher_vigenere(text: str, key: str) -> str:
     res = []
-    for i, char in enumerate(text):
-        modif = get_weight(key[i % len(key)])
-        c_num = get_weight(char)
-        res.append(CYRILLIC[(c_num + modif) % CYRILLIC_LEN])
-    return "".join(res)
+    key_i = 0
 
-def cipher_decimation(text: str, step: int) -> str:
-    length = len(text)
-    res = [text[(i * step) % length] for i in range(length)]
+    for char in text:
+        if char.lower() not in CYRILLIC:
+            res.append(char)
+            continue
+
+        is_upper = char.isupper()
+
+        modif = get_weight_cyrillic(key[key_i % len(key)])
+        c_num = get_weight_cyrillic(char)
+        new_char = CYRILLIC[(c_num + modif) % CYRILLIC_LEN]
+
+        if is_upper:
+            new_char = new_char.upper()
+
+        res.append(new_char)
+        key_i += 1
+
     return "".join(res)
 
 def decipher_vigenere(text: str, key: str) -> str:
     res = []
-    for i, char in enumerate(text):
-        modif = get_weight(key[i % len(key)])
-        c_num = get_weight(char)
-        res.append(CYRILLIC[(c_num - modif) % CYRILLIC_LEN])
+    key_i = 0
+
+    for char in text:
+        if char.lower() not in CYRILLIC:
+            res.append(char)
+            continue
+
+        is_upper = char.isupper()
+
+        modif = get_weight_cyrillic(key[key_i % len(key)])
+        c_num = get_weight_cyrillic(char)
+        new_char = CYRILLIC[(c_num - modif + CYRILLIC_LEN) % CYRILLIC_LEN]
+
+        if is_upper:
+            new_char = new_char.upper()
+
+        res.append(new_char)
+        key_i += 1
+
     return "".join(res)
+
+
+def cipher_decimation(text: str, step: int) -> str:
+    res = []
+
+    for char in text:
+        if char.lower() not in ENGLISH:
+            res.append(char)
+            continue
+
+        is_upper = char.isupper()
+
+        i = get_weight_english(char)
+        encrypted_pos = (i * step) % ENGLISH_LEN
+        new_char = ENGLISH[encrypted_pos]
+
+        if is_upper:
+            new_char = new_char.upper()
+
+        res.append(new_char)
+
+    return "".join(res)
+
 
 def mod_inverse(k: int, n: int) -> int:
     m0 = n
@@ -40,11 +91,9 @@ def mod_inverse(k: int, n: int) -> int:
     while k > 1:
         q = k // n
         t = n
-
         n = k % n
         k = t
         t = y
-
         y = x - q * y
         x = t
 
@@ -54,6 +103,24 @@ def mod_inverse(k: int, n: int) -> int:
     return x
 
 def decipher_decimation(text: str, step: int) -> str:
-    L = len(text)
-    inv_step = mod_inverse(step, L)
-    return "".join([text[(i * inv_step) % L] for i in range(L)])
+    inv_step = mod_inverse(step, ENGLISH_LEN)
+    res = []
+
+    for char in text:
+        if char.lower() not in ENGLISH:
+            res.append(char)
+            continue
+
+        is_upper = char.isupper()
+
+        i = get_weight_english(char)
+        decrypted_pos = (i * inv_step) % ENGLISH_LEN
+        new_char = ENGLISH[decrypted_pos]
+
+        if is_upper:
+            new_char = new_char.upper()
+
+        res.append(new_char)
+
+    return "".join(res)
+
