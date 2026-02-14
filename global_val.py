196a1ad390b3bc90f032f6dfdac1c032841990c2
GeneralBaks <vlad2273845@gmail.com>
2026-02-14 21:54:43 +0300

    Update global_val.py

--- global_val.py
+++ global_val.py
@@ -1,4 +1,6 @@
-CYRILLIC: str = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-ENGLISH: str = "abcdefghijklmnopqrstuvwxyz"
-NUMBERS: str = "1234567890"
-CYRILLIC_LEN = len(CYRILLIC)
+from typing import Final
+CYRILLIC: Final[str] = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+ENGLISH: Final[str] = "abcdefghijklmnopqrstuvwxyz"
+NUMBERS: Final[str] = "0123456789"
+CYRILLIC_LEN: Final[int] = len(CYRILLIC)
+ENGLISH_LEN: Final[int] = len(ENGLISH)
